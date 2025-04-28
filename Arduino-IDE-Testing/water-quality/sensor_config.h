ab8206a02f0f1fc498e601912e4b1e7f06282155
psword <101267297+psword@users.noreply.github.com>
2025-04-28 09:50:12 +0300

    updated files for final test proceduresand analysis

--- Arduino-IDE-Testing/water-quality/sensor_config.h
+++ Arduino-IDE-Testing/water-quality/sensor_config.h
@@ -52,6 +52,6 @@ extern const unsigned long readDelay; // Global constant for the delay between s
 #define READING_DURATION 4000                        // Duration in milliseconds for sensor sampling
 static const unsigned long uS_TO_S_FACTOR = 1000000; // Conversion factor from microseconds to seconds
 static const int S_TO_MIN_FACTOR = 60;               // Conversion factor from seconds to minutes
-static const int TIME_TO_SLEEP = 360;                 // Duration in minutes for deep sleep between readings
+static const int TIME_TO_SLEEP = 2;                 // Duration in minutes for deep sleep between readings
 
 #endif // SENSOR_CONFIG
